6e871a32fe199b199741a0bff718766d09b8f647
Nihadname <nihadcoding@gmail.com>
2024-08-15 04:39:47 +0400

    register

--- ShopApplcationBackEndApi/Apps/AdminApp/Controllers/AuthController.cs
+++ ShopApplcationBackEndApi/Apps/AdminApp/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ShopApplcationBackEndApi.Apps.AdminApp.Dtos.UserDto;
+using ShopApplcationBackEndApi.Entities;
 
 namespace ShopApplcationBackEndApi.Apps.AdminApp.Controllers
 {
@@ -7,10 +10,27 @@ namespace ShopApplcationBackEndApi.Apps.AdminApp.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpPost]
-        public async Task<IActionResult> Register()
+        private readonly UserManager<AppUser > _userManager;
+
+        public AuthController(UserManager<AppUser> userManager)
         {
-            return Ok();
+            _userManager = userManager;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var existUser = _userManager.FindByNameAsync(registerDto.UserName);
+            if (existUser != null) return BadRequest();
+            AppUser appUser = new AppUser();
+            appUser.UserName = registerDto.UserName;
+            appUser.Email = registerDto.Email;
+            appUser.FullName=registerDto.FullName;
+            var result= await _userManager.CreateAsync(appUser,registerDto.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+            await _userManager.AddToRoleAsync(appUser, "member");
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        
     }
 }
--- ShopApplcationBackEndApi/Data/Migrations/20240815003900_newOnes.Designer.cs
+++ ShopApplcationBackEndApi/Data/Migrations/20240815003900_newOnes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopApplcationBackEndApi.Data;
 
@@ -11,9 +12,11 @@ using ShopApplcationBackEndApi.Data;
 namespace ShopApplcationBackEndApi.Data.Migrations
 {
     [DbContext(typeof(ShopAppContext))]
-    partial class ShopAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240815003900_newOnes")]
+    partial class newOnes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
